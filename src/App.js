3615007f889fe6936d5138f4452b9c3237938ad9
Chris Kay <ckay84@protonmail.com>
2020-11-02 17:39:02 -0500

    Set board and player sizes in App, pass to GameContainer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import GameContainer from './containers/GameContainer';
 
 function App() {
+  const boardSize = window.visualViewport.width / 2
   return (
     <div className="App">
-      <GameContainer />
+      <GameContainer boardSize={boardSize} playerSize={50} />
     </div>
   );
 }
